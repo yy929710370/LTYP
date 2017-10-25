ffe043fa81ff2dee8e3e75684c9b3714aabeb812
yy929710370 <hjh0370@163.com>
2017-10-25 10:56:01 +0800

    day02测试1hjh

--- src/main/java/test/SendMailTest.java
+++ src/main/java/test/SendMailTest.java
@@ -4,6 +4,7 @@ import cn.vworld.utils.MailUtils;
 
 public class SendMailTest {
     public static void main(String[] args) throws Exception {
+        System.out.println("测试1000");
         MailUtils.sendMail("929710370@qq.com", "www.baidu.com");
     }
 }
